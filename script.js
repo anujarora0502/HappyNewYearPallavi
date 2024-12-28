9f542dc06148e8dfa10cb1acc7c5ea56631dbdb3
anujarora0502 <anujarora0502@gmail.com>
2024-12-28 06:15:47 +0530

    Nice try diddy

--- script.js
+++ script.js
@@ -1,8 +1,33 @@
-// document.addEventListener('DOMContentLoaded', function () {
-//     // Initialize Fireworks effect when the page loads
-//     // Fireworks effect on 'Celebrate' button click
-//     const celebrateButton = document.getElementById('celebrateButton');
-//     celebrateButton.addEventListener('click', function() {
-//         fireworks.start(); // Start the fireworks animation
-//     });
-// });
+document.addEventListener('DOMContentLoaded', function () {
+    // Get the container for fireworks
+    const container = document.querySelector('.fireworks');
+    
+    // Initialize fireworks
+    const fireworks = new Fireworks.default(container,{
+        speed: 4,
+        acceleration: 1.05,
+        friction: 1,
+        gravity: 4,
+        particles: 400,
+        explosion: 10
+    });
+    
+    // Variable to keep track of whether fireworks are active or not
+    let fireworksActive = false;
+
+    // Fireworks effect on 'Celebrate' button click
+    const celebrateButton = document.getElementById('celebrateButton');
+    celebrateButton.addEventListener('click', function() {
+        if (fireworksActive) {
+            fireworks.stop(); // Stop the fireworks animation if it's active
+            celebrateButton.textContent = 'Celebrate'
+            celebrateButton.style.backgroundColor = 'teal'
+        } else {
+            fireworks.start(); // Start the fireworks animation if it's not active
+             celebrateButton.textContent = 'Stop'
+             celebrateButton.style.backgroundColor = 'red'
+        }
+        // Toggle the status of fireworks
+        fireworksActive = !fireworksActive;
+    });
+});

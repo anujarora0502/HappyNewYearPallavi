aefa6c7c20b9af142d0b0d0ba070e767a3ac42d7
anujarora0502 <anujarora0502@gmail.com>
2024-12-28 05:52:40 +0530

    Nice try diddy

--- script.js
+++ script.js
@@ -1,4 +1,8 @@
-// You can add any additional interactivity here in the future if needed
-document.addEventListener('DOMContentLoaded', function () {
-    console.log('Welcome to Pallavi\'s Website!');
-});
+// document.addEventListener('DOMContentLoaded', function () {
+//     // Initialize Fireworks effect when the page loads
+//     // Fireworks effect on 'Celebrate' button click
+//     const celebrateButton = document.getElementById('celebrateButton');
+//     celebrateButton.addEventListener('click', function() {
+//         fireworks.start(); // Start the fireworks animation
+//     });
+// });
